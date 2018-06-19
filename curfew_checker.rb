d3900a131f5edbe31a131ee5de4febecfe8de03f
EmilyHenken <20henkee@corjesu.org>
2018-06-19 18:22:05 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -12,8 +12,8 @@ curfew_checker
 
   
 
-if Time.now.hour >= 23
-  puts "you're in trouble"
-else
-  puts "you can keep having fun"
-end
+# if Time.now.hour >= 23
+#   puts "you're in trouble"
+# else
+#   puts "you can keep having fun"
+# end
