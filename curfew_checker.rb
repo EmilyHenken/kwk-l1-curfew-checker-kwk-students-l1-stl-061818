5a76cf917a1567729e527c96572e8af6eafb648f
EmilyHenken <20henkee@corjesu.org>
2018-06-19 18:23:47 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,19 +1,19 @@
+time_left = 11 - Time.now.hour
+
 simple_curfew_checker
 if Time.now.hour = 23
   puts "it is time to go to bed"
   elsif Time.now.hour > 23
   puts "you're in trouble"
 else
-  puts "You can keep having fun"
+  puts "You have #{time_left} until curfew."
 end
   
   
 curfew_checker
 
-  
 
-# if Time.now.hour >= 23
-#   puts "you're in trouble"
-# else
-#   puts "you can keep having fun"
-# end
+
+
+
+
